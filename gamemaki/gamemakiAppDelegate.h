e3ea6a6c4cf1786f683abd7d9ea9a2e94766ff8d
Socialico <socialico@MacBookPro-office.local>
2011-05-24 18:12:46 +0800

    Initial Commit

---
+++ gamemaki/gamemakiAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  gamemakiAppDelegate.h
+//  gamemaki
+//
+//  Created by Socialico on 5/24/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface gamemakiAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
